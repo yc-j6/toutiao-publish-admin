5462b00de8c61e9642a21cf69ee44273f111409a
yc-j6 <79829752+yc-j6@users.noreply.github.com>
2021-05-11 11:54:58 +0800

    Update index.vue

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+  <img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Finews.gtimg.com%2Fnewsapp_match%2F0%2F12020722665%2F0.jpg&refer=http%3A%2F%2Finews.gtimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1623295742&t=81bec6c8f21e20a047d30a3085088917" alt="" srcset="">
+  </div>
 </template>
 
 <script>
 </script>
 
 <style>
+.home-container{
+  padding: 0;
+}
+.home-container,.home-container img{
+  width: 100%;
+  height: 100%;
+}
 </style>
